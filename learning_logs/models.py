2920fe3b95192f7e03169c438e6339928fcfae95
Dylangomezz <dylangomesf@gmail.com>
2026-09-11 15:19:06 -0300

    Aula 2 curso django

---
+++ learning_logs/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Topic(models.Model):
+    """Um assunto sobre o qual o usuário está aprendendo."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __strt__(self):
+        """Devolve uma representação de uma string do modelo."""
+        return self.text
